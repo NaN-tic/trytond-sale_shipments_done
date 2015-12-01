3fa9d9c6b5d9893396f67377c50a61a70f81d420
resteve <resteve@zikzakmedia.com>
2015-12-01 11:22:35 +0100

    Tests

--- tests/test_sale_shipments_done.py
+++ tests/test_sale_shipments_done.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part sale_shipments_done module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the sale_shipments_done module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleShipmentsDoneTestCase(unittest.TestCase):
+class SaleShipmentsDoneTestCase(ModuleTestCase):
     'Test Sale Shipments Done module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_shipments_done')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_shipments_done')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_shipments_done'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleShipmentsDoneTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
